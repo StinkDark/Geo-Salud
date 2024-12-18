be90d104631e58360beabc7ef8124b7e5010d56d
SamDvid <samuelrudas90@gmail.com>
2024-12-18 17:21:41 -0500

    Update tailwind.config.js
    añadiendo apartado de screens para aplicar responsive web design en tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,5 +28,19 @@ module.exports = {
     //     '4xl': '2rem',
     //   }
     // }
+
+    screens: {
+      'smartphones': '320px',
+       // => @media (min-width: 320px) { ... }
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   }
 }
+
